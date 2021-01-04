1650105b6f760a3d97553c2354365bd173799e8f
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-01-04 17:38:55 +0300

    add

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyDsN42sfvXTz4C2WwN0cTZzPq0iJAug34Q',
-  authDomain: 'softlecure.firebaseapp.com',
-  projectId: 'softlecure',
-  storageBucket: 'softlecure.appspot.com',
-  messagingSenderId: '482012232864',
-  appId: '1:482012232864:web:d2d4c30d4333ec8f785dc5',
-  measurementId: 'G-JCPFL2WP08',
+  apiKey: 'AIzaSyC5lc20Iya8hRKze1OhRsKSOSRWnV156f4',
+  authDomain: 'softlecture-1858b.firebaseapp.com',
+  projectId: 'softlecture-1858b',
+  storageBucket: 'softlecture-1858b.appspot.com',
+  messagingSenderId: '425114176815',
+  appId: '1:425114176815:web:36626802eb68a70e2ce5da',
+  measurementId: 'G-H2GRCGJSRX',
 });
 
 const db = firebaseApp.firestore();
