c259aead08c6cdbd17079a521c5a54954235460b
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-01-04 10:55:02 +0300

    add avatar

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,29 +1,72 @@
 import React from 'react';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 function About() {
   return (
     <div className="container">
       <h1>About Page</h1>
       <div className="row mt-4">
-        <h2 className="mb-2 text-lg text-gray-600">Developed By:</h2>
-        <div className="border border-gray-300 shadow rounded-lg p-6 col-sm-6  col-lg-4">
-          <div className="flex justify-between items-center sm:px-4 lg:px-6">
-            <h3>Abdulbasit Salah</h3>
-            <img
-              src="https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png"
-              alt=""
-              className="rounded-full w-20"
-            />
+        <h2 className="mb-2 text-lg text-gray-600">
+          Developed && Designed By:
+        </h2>
+        <div className="mt-2 flex items-center ">
+          <div className="p-4 rounded-lg shadow-md w-2/6 px-8 mr-6">
+            <div className="flex justify-between items-center">
+              <div>
+                <h1 className="text-2xl font-semibold text-indigo-400">
+                  Abdulbasit Salah
+                </h1>
+                <div className="flex items-center">
+                  <p className="text-base mr-3 text-gray-600 text-medium">
+                    Full-Stack Web Developer
+                  </p>
+                  <a
+                    href="https://github.com/abdulbasit-web"
+                    rel="noreferrer"
+                    target="_blank"
+                    className="hover:text-indigo-500"
+                  >
+                    <GitHubIcon />
+                  </a>
+                </div>
+              </div>
+              <div class="relative w-16 h-16">
+                <img
+                  className="rounded-full border border-gray-100 shadow-sm"
+                  src="basit.png"
+                  alt="user avatar"
+                />
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="border border-gray-300 shadow rounded-lg p-6 col-sm-6  col-lg-4">
-          <div className="flex justify-between items-center sm:px-4 lg:px-6">
-            <h3>Abdulbasit Salah</h3>
-            <img
-              src="https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png"
-              alt=""
-              className="rounded-full w-20"
-            />
+          <div className="p-4 rounded-lg shadow-md w-2/6 px-8">
+            <div className="flex justify-between items-center">
+              <div>
+                <h1 className="text-2xl font-semibold text-indigo-400">
+                  Hewr Srood
+                </h1>
+                <div className="flex items-center">
+                  <p className="text-base mr-3 text-gray-600 text-medium">
+                    Front-End Web Developer
+                  </p>
+                  <a
+                    href="https://github.com/Hewr-Srood"
+                    rel="noreferrer"
+                    target="_blank"
+                    className="hover:text-indigo-500"
+                  >
+                    <GitHubIcon />
+                  </a>
+                </div>
+              </div>
+              <div class="relative w-16 h-16">
+                <img
+                  className="rounded-full border border-gray-100 shadow-sm"
+                  src="hewr.png"
+                  alt="user avatar"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
