ce42592daa8eb5879898d792f5d8bab006f86d10
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-01-14 08:36:04 +0300

    finsih styling

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import emailjs from "emailjs-com";
 
+import send from "../assets/images/bermuda-sending-e-mail.svg";
+import { Button, TextField } from "@material-ui/core";
+
 function Feedback() {
+  const [error, setError] = useState(false);
   function sendEmail(e) {
     e.preventDefault();
 
@@ -25,67 +29,66 @@ function Feedback() {
 
   return (
     <div>
-      <div class="relative flex items-top justify-center min-h-screen bg-white dark:bg-gray-900 sm:items-center sm:pt-0">
+      <div class="flex items-top justify-center sm:items-center sm:pt-0">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
           <div class="mt-8 overflow-hidden">
-            <div class="grid grid-cols-1 md:grid-cols-2">
-              <div class="p-6 mr-2 bg-gray-100 dark:bg-gray-800 sm:rounded-lg">
-                <h1 class="text-4xl sm:text-5xl text-gray-800 dark:text-white font-extrabold tracking-tight">
+            <div class="grid grid-cols-1 md:grid-cols-2  items-center">
+              <div class="p-6 mr-2   sm:rounded-lg ">
+                <h1 class="text-3xl sm:text-5xl text-gray-700 dark:text-white font-semibold tracking-tight">
                   Send A message
                 </h1>
-                <p class="text-normal text-lg sm:text-2xl font-medium text-gray-600 dark:text-gray-400 mt-2">
-                  for any issue or adding lecture please tall us to ipvore the
-                  app
+                <p class="text-normal text-lg sm:text-2xl font-medium text-gray-500  mt-3">
+                  for any issue or adding lecture please tall us to impvore the
+                  project
                 </p>
+                <form className="mt-6" onSubmit={sendEmail}>
+                  <TextField
+                    type="text"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    label="Your name"
+                    name="name"
+                    autoFocus
+                  />
+                  <TextField
+                    type="email"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    label="Email Address"
+                    name="email"
+                    autoComplete="email"
+                  />
+                  <TextField
+                    type="text"
+                    required
+                    variant="outlined"
+                    margin="normal"
+                    multiline
+                    rows={5}
+                    rowsMax={8}
+                    fullWidth
+                    label="Your message"
+                    name="message"
+                  />
+                  <div className="mt-2">
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      fullWidth={false}
+                      size="large"
+                    >
+                      Send Feedback
+                    </Button>
+                  </div>
+                </form>
               </div>
 
-              <form
-                class="p-6 flex flex-col justify-center"
-                onSubmit={sendEmail}
-              >
-                <div class="flex flex-col">
-                  <label for="name" class="hidden">
-                    Full Name
-                  </label>
-                  <input
-                    type="name"
-                    name="name"
-                    id="name"
-                    placeholder="Full Name"
-                    class="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                  />
-                </div>
-
-                <div class="flex flex-col mt-2">
-                  <label for="email" class="hidden">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    class="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                  />
-                </div>
-                <div class="flex flex-col mt-2">
-                  <label for="message" class="hidden">
-                    Message
-                  </label>
-                  <textarea
-                    required
-                    name="message"
-                    placeholder="enter your message"
-                    class="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                  ></textarea>
-                </div>
-
-                <button
-                  type="submit"
-                  class="md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300"
-                >
-                  Submit
-                </button>
-              </form>
+              <div className="mb-16 md:mb-0">
+                <img src={send} alt="cover" />
+              </div>
             </div>
           </div>
         </div>
