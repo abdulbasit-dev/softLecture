0cc6dd479df3efe3a8ed4d01638ea7ce75f7340f
abdulbasit-web <abdullbasit99salah@gmail.com>
2021-01-14 08:47:34 +0300

    handle required message feild

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -9,36 +9,40 @@ function Feedback() {
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_6bzwgo6",
-        "template_za0c2qd",
-        e.target,
-        "user_EsXxuYomDwAzvrHvWnB75"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    e.target.reset();
+    if (!e.target.message.value) {
+      setError(true);
+    } else {
+      emailjs
+        .sendForm(
+          "service_6bzwgo6",
+          "template_za0c2qd",
+          e.target,
+          "user_EsXxuYomDwAzvrHvWnB75"
+        )
+        .then(
+          (result) => {
+            alert("Than you, your message was send");
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      e.target.reset();
+    }
   }
 
   return (
     <div>
-      <div class="flex items-top justify-center sm:items-center sm:pt-0">
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-          <div class="mt-8 overflow-hidden">
-            <div class="grid grid-cols-1 md:grid-cols-2  items-center">
-              <div class="p-6 mr-2   sm:rounded-lg ">
-                <h1 class="text-3xl sm:text-5xl text-gray-700 dark:text-white font-semibold tracking-tight">
+      <div className="flex items-top justify-center sm:items-center sm:pt-0">
+        <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+          <div className="mt-8 overflow-hidden">
+            <div className="grid grid-cols-1 md:grid-cols-2  items-center">
+              <div className="p-6 mr-2   sm:rounded-lg ">
+                <h1 className="text-3xl sm:text-5xl text-gray-700 dark:text-white font-semibold tracking-tight">
                   Send A message
                 </h1>
-                <p class="text-normal text-lg sm:text-2xl font-medium text-gray-500  mt-3">
-                  for any issue or adding lecture please tall us to impvore the
+                <p className="text-normal text-lg sm:text-2xl font-medium text-gray-500  mt-3">
+                  for any issue or adding lecture please tell us to impvore the
                   project
                 </p>
                 <form className="mt-6" onSubmit={sendEmail}>
@@ -63,6 +67,7 @@ function Feedback() {
                   <TextField
                     type="text"
                     required
+                    error={error}
                     variant="outlined"
                     margin="normal"
                     multiline
@@ -71,7 +76,9 @@ function Feedback() {
                     fullWidth
                     label="Your message"
                     name="message"
+                    helperText={error && "please fill message field "}
                   />
+
                   <div className="mt-2">
                     <Button
                       type="submit"
